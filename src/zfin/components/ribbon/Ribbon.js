1c5cbb06fbdc02c2a0e745fcefdb9ee4f1c30c08
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-01-20 16:26:25 -0800

    use wc-ribbon-strips in place of genericribbon

--- src/zfin/components/ribbon/Ribbon.js
+++ src/zfin/components/ribbon/Ribbon.js
@@ -1,7 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import GenericRibbon from '../../../components/GenericRibbon.js';
+import React, { useEffect, useRef, useState } from 'react';
+import { applyPolyfills, defineCustomElements } from '@geneontology/wc-ribbon-strips/loader';
 
 import style from './style.scss';
+
+applyPolyfills().then(() => {
+    defineCustomElements(window);
+});
 
 const Ribbon = (props) => {
     const ribbonRef = useRef(null);
@@ -23,12 +27,14 @@ const Ribbon = (props) => {
     );
     return (
         <div className='ontology-ribbon-container horizontal-scroll-container' ref={ribbonRef}>
-            <GenericRibbon
-                hideFirstSubjectLabel
-                colorBy={1} // annotations
-                binaryColor
-                maxColor={[style.primaryR, style.primaryG, style.primaryB]}
-                {...props}
+            <wc-ribbon-strips
+                hide-first-subject-label
+                color-by='1' // annotations
+                binary-color
+                max-color={[style.primaryR, style.primaryG, style.primaryB]}
+                update-on-subject-change={false}
+                subject-position='0'
+                data={JSON.stringify(props)}
             />
             <small className='text-muted'>
                 Click a filled square to see annotations
